4850f82c9a7b296f846ffd910461a3b007d54e47
Codeco-Sobirjon <sobirbobojonov05763104@gmail.com>
2024-10-17 18:56:51 +0500

    all libraries to added into .txt

--- config/settings/server.py
+++ config/settings/server.py
@@ -2,5 +2,16 @@ from .base import *
 
 
 DEBUG = True
+
 ALLOWED_HOSTS = ['*']
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'irrigatsiya',
+        'USER': 'postgres',
+        "PASSWORD": "1",
+        "HOST": "localhost",
+        "PORT": 5432,
+    }
+}
